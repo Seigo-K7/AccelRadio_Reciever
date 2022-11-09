f9b04c0c2db184546f9d135d495e975832bf5914
Seigo-K7 <117812600+Seigo-K7@users.noreply.github.com>
2022-11-09 20:39:39 +0900

    pxt.json, main.blocks, main.ts, main.py 업데이트하기

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ music.play_melody("C6 C6 c6", 200)
 radio.set_group(1)
 
 def on_received_number(receivedNumber):
-    
-    #serial.write_value("Acc", receivedNumber)
-
+    # serial.write_value("Acc", receivedNumber)
     serial.write_number(receivedNumber)
-    led.plot_bar_graph(receivedNumber, 1024)
+    serial.write_string("\n")
+    #led.plot_bar_graph(receivedNumber, 1024)
+
+
+
 radio.on_received_number(on_received_number)
 
--- main.ts
+++ main.ts
@@ -1,8 +1,9 @@
 basic.showIcon(IconNames.Yes)
 music.playMelody("C6 C6 c6", 200)
 radio.setGroup(1)
+// led.plot_bar_graph(receivedNumber, 1024)
 radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
-    // serial.write_value("Acc", receivedNumber)
+    //  serial.write_value("Acc", receivedNumber)
     serial.writeNumber(receivedNumber)
-    led.plotBarGraph(receivedNumber, 1024)
+    serial.writeString("\n")
 })
